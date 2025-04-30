51b45115c5206ebcad5c624c846599226236d86b
Rodrigo Barbosa <rodrigo.barbosa@veplex.com.br>
2025-04-30 15:43:19 -0300

    teste dnv

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,7 @@
 import Link from 'next/link';
 function Home() {
-  return <h1>Teste</h1>;
+  return <h1>Teste222</h1>;
 }
-// export default function Home() {
-//   return (  
-//     <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
-//       <h1>Replicate API Integration</h1>
-//       <Link href="/process">
-//         <button>Go to Processing Page</button>
-//       </Link>
-//     </div>
-//   );
-// }
 
 function teste() {
   console.log("testao");
