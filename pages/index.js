79a0c427dd72e562b64c87cf5f54b9c7a79b339f
Rodrigo Barbosa <rodrigo.barbosa@veplex.com.br>
2025-04-30 15:42:07 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,16 @@ import Link from 'next/link';
 function Home() {
   return <h1>Teste</h1>;
 }
-export default function Home() {
-  return (  
-    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
-      <h1>Replicate API Integration</h1>
-      <Link href="/process">
-        <button>Go to Processing Page</button>
-      </Link>
-    </div>
-  );
-}
+// export default function Home() {
+//   return (  
+//     <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+//       <h1>Replicate API Integration</h1>
+//       <Link href="/process">
+//         <button>Go to Processing Page</button>
+//       </Link>
+//     </div>
+//   );
+// }
 
 function teste() {
   console.log("testao");
